f354acee53d408fd16de795a7a7a74ab9bf177d1
Matheus Faria de Alencar <mtsalenc@gmail.com>
2018-02-06 22:18:52 -0200

    feat: Clear up protocol design

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import utils from 'ethereumjs-util'
 const ec = new EdDSA('ed25519');
 const G = [ec.g.x,ec.g.y];
 
-//Bob
+//Candidate X keys publishes A and B
 const aKeys = ec.keyFromSecret("spendKey");
 const a = aKeys.priv();
 const A = aKeys.pub();
@@ -15,22 +15,27 @@ const bKeys = ec.keyFromSecret("viewKey");
 const b = bKeys.priv();
 const B = bKeys.pub();
 
-//Bob gives A and B to Alice
-
-//Alice computes a random key
-const rKeys = ec.keyFromSecret("rKeys");
+//Voter computes a random key
+const rKeys = ec.keyFromSecret(crypto.randomBytes(64).toString('hex'));
 const r = rKeys.priv();
 const R = rKeys.pub();
 
-//Alice computes P with bobs A and B:
+//Voter computes P with Candidate X's public keys
 //P=H(rA)G+B
+const P = ec.g.mul(new BN(H(A.mul(r).encode('hex')))).add(B).encode('hex');
 //and signs a transaction to P, publishing R.
 
-//Bob checks every transaction
+//Candidate X checks every transaction
 //_P=H(aR)G+B
-//Once he finds _P == P he knows he is the recipient of that tx
-console.log(A.mul(r));
-console.log(R.mul(a));
+const _P = ec.g.mul(new BN(H(R.mul(a).encode('hex')))).add(B).encode('hex');
+//Once he finds _P == P he knows he is the recipient of that vote
+console.log(P===_P);
+
+//He can calculate the corresponding private key _p using his private keys
+//_p = H(aR) + b
+const _p = new BN(H(R.mul(a).encode('hex'))).add(b);
+console.log(ec.g.mul(_p).encode('hex')===_P);
+
 
 function H(input){
   return keccakHash('keccak256').update(input).digest('hex');
